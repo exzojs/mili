a683e121701378119123d2b78b6de197705e5fa3
Серков Евгений <exzojs@gmail.com>
2022-07-18 13:08:19 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    int amount = 5000;
+    int mili = amount / 20;
+    System.out.println(mili + " Бонусных миль");
+
+
+
+
+
+
+
+
+
+
+    }
+}
